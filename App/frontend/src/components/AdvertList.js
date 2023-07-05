243a58f10ca1fe29e45630f568b932605944d8d4
Teemu <teemuknuutila@outlook.com>
2023-07-05 19:52:00 +0300

    test

--- App/frontend/src/components/AdvertList.js
+++ App/frontend/src/components/AdvertList.js
@@ -1,21 +1,16 @@
-import AdCard from './AdCard';
+import AdCard from "./AdCard";
 
 const AdvertList = (props) => {
+  let items = props.list.map((item, index) => {
+    return <AdCard key={item._id} item={item} index={index} />;
+  });
 
-    let items = props.list.map((item,index) => {
-      
-        return(
-            <AdCard key={item._id} item={item} index={index} />
-        )
-    })
-
-    return(
-       <div>
-        <h3>Ilmoitukset</h3>
-        {items}
-        </div>
-    )
-
-}
+  return (
+    <div>
+      <h3>Ilmoitukset</h3>
+      {items}
+    </div>
+  );
+};
 
 export default AdvertList;
